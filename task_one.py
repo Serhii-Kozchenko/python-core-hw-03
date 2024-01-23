caac0a51cbad0dd0658c03852cb26257dc7207e1
Serhii-Kozchenko <serhii7534@i.ua>
2024-01-23 14:19:44 +0200

    Add task_one

---
+++ task_one.py
@@ -0,0 +1,20 @@
+# Створіть функцію get_days_from_today(date), яка розраховує кількість днів між заданою датою і поточною датою.
+
+from datetime import datetime
+
+
+def get_days_from_today(date: str):
+
+    try:
+        date_today = datetime.today()
+        calculation_date = datetime.strptime(date, '%Y-%m-%d')
+        result = calculation_date.toordinal() - date_today.toordinal()
+        return f"До {date} залишилось: {result} днів"
+
+    except Exception as error:
+        print(f"Error: {error}")
+
+
+print(get_days_from_today('2024-03-03'))
+print(get_days_from_today('2024-01-01'))
+print(get_days_from_today(2024))
